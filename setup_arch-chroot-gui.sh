37574c2abb6e254cfd70c1d631585caff6f3c0c4
Carlos Junior <cjuniorfox@gmail.com>
2019-05-15 23:13:36 -0300

    configuration XORG

--- setup_arch-chroot-gui.sh
+++ setup_arch-chroot-gui.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-  echo -e "\n\ny" | pacman -S xorg-xdm openbox xorg virt-manager xterm firefox font-bh-ttf ttf-dejavu  ttf-liberation
+  echo -e "\n\ny" | pacman -S xorg-xdm xorg-xinit xorg xterm xorg-xclock xorg-twm \
+    firefox virt-manager \
+  	font-bh-ttf ttf-dejavu ttf-liberation \
+  	openbox  gnome-terminal \
+  	
 #  systemctl enable xdm.service
-  echo ‘openbox’ > ~/.xsession
-  chmod +x ~/.xsession
+  echo ‘exec openbox-session’ > ~/.xinitrc
   LANG=C pacman -Sl | awk '/\[installed\]$/ {print $1 "/" $2 "-" $3}' > /pkglist.txt
   yes | pacman -Scc
